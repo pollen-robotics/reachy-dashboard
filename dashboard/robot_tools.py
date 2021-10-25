a84f121c6847c808011851fe7701eccb14331d23
simheo <32677536+simheo@users.noreply.github.com>
2021-10-25 14:31:50 +0200

    Adjust config.

--- dashboard/robot_tools.py
+++ dashboard/robot_tools.py
@@ -1,9 +1,12 @@
 import os
 import yaml
 
+from subprocess import check_output
+
 from reachy_pyluos_hal.discovery import identify_luos_containers, find_gate
 
 DEFAULT_MODEL = 'full_kit'
+DEFAULT_CARDS = 'camera'
 
 def identify_model():
     model = os.getenv('REACHY_MODEL')
@@ -19,6 +22,18 @@ def identify_model():
 
         model = config['model']
     return model
+
+def identify_cards():
+    with open('~/.boards.yaml') as f:
+        card_config = yaml.load(f, Loader=yaml.FullLoader)
+        if 'boards' not in card_config:
+            required_cards = DEFAULT_CARDS
+        required_cards = card_config['boards']
+
+    with open(os.path.abspath('..')+'/boards/configs/'+required_cards+'.yaml') as f: 
+        config = yaml.load(f, Loader=yaml.FullLoader) 
+    check_output()    
+
 
 def check_missing_motors():
     port, found, missing = find_gate()
