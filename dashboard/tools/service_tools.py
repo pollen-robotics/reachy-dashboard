c968153b12c2ad5ec2c91b715683c972cf70a8b4
simheo <32677536+simheo@users.noreply.github.com>
2022-01-03 15:47:16 +0100

    Remove .service extension from service name.

--- dashboard/tools/service_tools.py
+++ dashboard/tools/service_tools.py
@@ -8,12 +8,12 @@ def list_services():
     pipe3 = run(["awk '{print $1}'"], input=pipe2.stdout, stdout=PIPE, shell=True)
 
     output = pipe3.stdout.decode().split('\n')
-    return [service for service in output if service]
-
+    service_list = [service for service in output if service]
+    return [srv_name.split('.service')[0] for srv_name in service_list]
 
 def restart_service(service: str):
-    run([f'sudo systemctl restart {service}'], stdout=PIPE, shell=True)
+    run([f'sudo systemctl restart {service}.service'], stdout=PIPE, shell=True)
 
 
 def stop_service(service: str):
-    run([f'sudo systemctl stop {service}'], stdout=PIPE, shell=True)
+    run([f'sudo systemctl stop {service}.service'], stdout=PIPE, shell=True)
