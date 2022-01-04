09e77da60169ea597b166d631f3796b5104abc95
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2022-01-04 16:43:23 +0100

    Change user name.

--- launcher.bash
+++ launcher.bash
@@ -1,5 +1,5 @@
 source /opt/ros/foxy/setup.bash
-source /home/pierre/reachy_ws/install/setup.bash
-source /home/pierre/ros_ws/install/setup.bash
+source /home/reachy/reachy_ws/install/setup.bash
+source /home/reachy/ros_ws/install/setup.bash
 
-python3 /home/pierre/dev/RAP-2021/dashboard/server.py
+python3 /home/reachy/dev/RAP-2021/dashboard/server.py
