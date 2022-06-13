db54f02608b47256b8bf6493c3a7dbe85870662b
simheo <32677536+simheo@users.noreply.github.com>
2022-06-13 10:24:23 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""Setup script."""
+
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+
+setup(
+    name='reachy_access_point',
+    version='0.1',
+    packages=find_packages(exclude=['tests']),
+
+    install_requires=['flask', 'aioflask'],
+
+    extra_requires={
+        'test': ['pytest'],
+    },
+
+    author='Pollen Robotics',
+    author_email='contact@pollen-robotics.com',
+    url='https://github.com/pollen-robotics/RAP-2021',
+
+    description="Dashboard web interface for Reachy 2021.",
+)
