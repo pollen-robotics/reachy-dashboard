74137dbb9c77bf6e54fbcab8998b62ac4b7db696
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2022-08-11 12:34:21 +0200

    Update service file to be used in --user mode.

--- generate-rap-service-file.bash
+++ generate-rap-service-file.bash
@@ -1,14 +1,8 @@
-tee rap.service <<EOF
+tee reachy_rap.service <<EOF
 [Unit]
 Description=Reachy Access Point service
-Wants=network-online.target
-After=network.target network-online.target
 [Service]
-PIDFile=/var/run/rap.pid
 ExecStart=/usr/bin/python3.8 $PWD/dashboard/server.py
-User=$(whoami)
-Group=$(whoami)
-Type=simple
 [Install]
-WantedBy=multi-user.target
+WantedBy=default.target
 EOF
