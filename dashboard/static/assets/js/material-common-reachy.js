45d8a4a500f5e2bc926831da3e6b5b30d6c0c56f
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2023-05-08 14:51:52 +0200

    Move common js function to material-common-reachy.js.

--- dashboard/static/assets/js/material-common-reachy.js
+++ dashboard/static/assets/js/material-common-reachy.js
@@ -5,3 +5,16 @@ const shutdown =  () => {
     request.open("GET", "/api/shutdown");
     request.send();
 }
+
+getRobotConfig = () => {
+    const footer = document.getElementById("footer-container");
+    const request = new XMLHttpRequest();
+  
+    request.onload = e => {
+        const config = JSON.parse(request.response);
+        footer.innerHTML = `Reachy configuration: ${config['model']} <br />`;
+        footer.innerHTML += `Reachy serial number: ${config['serial_number']}`;
+    }
+    request.open("GET", "/api/get-reachy-info", true);
+    request.send();
+  }
