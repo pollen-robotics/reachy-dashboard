8e0e331e544e3a499b758226c95da6018feeb7bd
Clément Dussieux <clement.dussieux@pollen-robotics.com>
2023-06-23 10:19:17 +0200

    wip

--- dashboard/server.py
+++ dashboard/server.py
@@ -2,7 +2,7 @@
 import os
 import json
 
-from flask import Flask, request, redirect, url_for, render_template, Response
+from flask import Flask, request, redirect, url_for, render_template, Response, jsonify
 
 from reachy_utils.config import get_reachy_model, get_reachy_generation, get_reachy_serial_number
 
@@ -21,301 +21,390 @@ else:
     print("Could not get Reachy's generation in configuration file.")
     exit()
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path="")
 app.secret_key = os.urandom(24)
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+reachy_dashboard = dashboard_tools.ReachyDashboard()
 
 
 # Render templates
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/wifi')
+@app.route("/wifi")
 def wifi():
     global wifi_list
-    if not net_tools.get_connection_status()['mode'] == 'Hotspot':
+    if not net_tools.get_connection_status()["mode"] == "Hotspot":
         wifi_list = net_tools.get_available_wifis()
     return render_template(
-        'wifi.html',
+        "wifi.html",
         wifi_list=wifi_list,
     )
 
 
-@app.route('/service')
+@app.route("/service")
 def service():
-    return render_template('service.html')
+    return render_template("service.html")
 
 
-@app.route('/app')
+@app.route("/app")
 def reachy_app():
-    return render_template('app.html')
+    return render_template("app.html")
 
 
-@app.route('/dashboard')
+@app.route("/dashboard")
 def dashboard():
     global reachy_dashboard
-    reachy_dashboard = dashboard_tools.ReachyDashboard()
     if not reachy_dashboard.connection_succeed:
-        print('Could not connect to Reachy')
+        logging.info("Could not connect to Reachy")
     else:
-        print('Connected to Reachy.')
-    return render_template('dashboard.html')
+        logging.info("Connected to Reachy.")
+    return render_template("dashboard.html")
 
 
 # Wifi API
-@app.route('/update-wifi', methods=['POST'])
+@app.route("/update-wifi", methods=["POST"])
 def update_wifi():
-    if net_tools.get_connection_status()['mode'] == 'Hotspot':
-        net_tools.set_hotspot_state('off')
+    if net_tools.get_connection_status()["mode"] == "Hotspot":
+        net_tools.set_hotspot_state("off")
 
-    net_tools.setup_new_wifi(request.form['ssid'], request.form['password'])
-    return redirect(url_for('wifi'))
+    net_tools.setup_new_wifi(request.form["ssid"], request.form["password"])
+    return redirect(url_for("wifi"))
 
 
-@app.route('/api/ip')
+@app.route("/api/ip")
 def update_ip():
     ip = net_tools.get_ip()
     net_tools.display_ip(ip)
     return Response(
         response=json.dumps(ip),
-        mimetype='application/json',
+        mimetype="application/json",
     )
 
 
-@app.route('/api/connection_status')
+@app.route("/api/connection_status")
 def update_connection_status():
     return Response(
         response=json.dumps(net_tools.get_connection_status()),
-        mimetype='application/json',
+        mimetype="application/json",
     )
 
 
-@app.route('/api/hotspot', methods=['POST'])
+@app.route("/api/hotspot", methods=["POST"])
 def toggle_hotspot():
     net_tools.set_hotspot_state(state=request.data.decode())
-    if request.data.decode() == 'off' and net_tools.get_connection_status()['mode'] == 'None':
-        net_tools.set_hotspot_state('on')
+    if request.data.decode() == "off" and net_tools.get_connection_status()["mode"] == "None":
+        net_tools.set_hotspot_state("on")
     return Response(status=200)
 
 
-@app.route('/api/available_networks')
+@app.route("/api/available_networks")
 def update_available_networks():
-    if net_tools.get_connection_status()['mode'] == 'Hotspot':
+    if net_tools.get_connection_status()["mode"] == "Hotspot":
         global wifi_list
         available_networks = wifi_list
     else:
         available_networks = net_tools.get_available_wifis()
     return Response(
         response=json.dumps(available_networks),
-        mimetype='application/json',
+        mimetype="application/json",
     )
 
 
-@app.route('/api/connection_card_info')
+@app.route("/api/connection_card_info")
 def update_connection_card_info():
     return Response(
         response=json.dumps(net_tools.get_connection_card_info()),
-        mimetype='application/json',
+        mimetype="application/json",
     )
 
 
 # Index API
-@app.route('/api/missing_modules_names')
+@app.route("/api/missing_modules_names")
 def get_missing_modules_names():
     return Response(
         response=json.dumps(debug_tools.get_missing_modules_names()),
-        mimetype='application/json',
+        mimetype="application/json",
     )
 
 
-@app.route('/api/missing_modules_bool')
+@app.route("/api/missing_modules_bool")
 def get_missing_modules_bool():
     return Response(
         response=json.dumps(debug_tools.are_missing_modules()),
-        mimetype='application/json',
+        mimetype="application/json",
     )
 
 
 # Service API
-@app.route('/api/list_services')
+@app.route("/api/list_services")
 def list_services():
     return Response(
-        response=json.dumps(service_app_tools.list_services_or_apps(type='service')),
-        mimetype='application/json',
+        response=json.dumps(service_app_tools.list_services_or_apps(type="service")),
+        mimetype="application/json",
     )
 
 
-@app.route('/api/restart_service', methods=['POST'])
+@app.route("/api/restart_service", methods=["POST"])
 def restart_service():
-    service_app_tools.restart_service_or_app(type='service', name=request.data.decode())
+    service_app_tools.restart_service_or_app(type="service", name=request.data.decode())
     return Response(status=200)
 
 
-@app.route('/api/stop_service', methods=['POST'])
+@app.route("/api/stop_service", methods=["POST"])
 def stop_service():
-    service_app_tools.stop_service_or_app(type='service', name=request.data.decode())
+    service_app_tools.stop_service_or_app(type="service", name=request.data.decode())
     return Response(status=200)
 
 
-@app.route('/api/is_service_running', methods=['POST'])
+@app.route("/api/is_service_running", methods=["POST"])
 def is_service_running():
     return Response(
-        response=json.dumps(service_app_tools.is_service_or_app_running(type='service', name=request.data.decode())),
-        mimetype='application/json',
+        response=json.dumps(service_app_tools.is_service_or_app_running(type="service", name=request.data.decode())),
+        mimetype="application/json",
     )
 
 
-@app.route('/api/status_service', methods=['POST'])
+@app.route("/api/status_service", methods=["POST"])
 def status_service():
-    return service_app_tools.get_service_or_app_status(type='service', name=request.data.decode())
+    return service_app_tools.get_service_or_app_status(type="service", name=request.data.decode())
 
 
 # App API
-@app.route('/api/list_apps')
+@app.route("/api/list_apps")
 def list_apps():
     return Response(
-        response=json.dumps(service_app_tools.list_services_or_apps(type='app')),
-        mimetype='application/json',
+        response=json.dumps(service_app_tools.list_services_or_apps(type="app")),
+        mimetype="application/json",
     )
 
 
-@app.route('/api/restart_app', methods=['POST'])
+@app.route("/api/restart_app", methods=["POST"])
 def restart_app():
-    service_app_tools.restart_service_or_app(type='app', name=request.data.decode())
+    service_app_tools.restart_service_or_app(type="app", name=request.data.decode())
     return Response(status=200)
 
 
-@app.route('/api/stop_app', methods=['POST'])
+@app.route("/api/stop_app", methods=["POST"])
 def stop_app():
-    service_app_tools.stop_service_or_app(type='app', name=request.data.decode())
+    service_app_tools.stop_service_or_app(type="app", name=request.data.decode())
     return Response(status=200)
 
 
-@app.route('/api/is_app_running', methods=['POST'])
+@app.route("/api/is_app_running", methods=["POST"])
 def is_app_running():
     return Response(
-        response=json.dumps(service_app_tools.is_service_or_app_running(type='app', name=request.data.decode())),
-        mimetype='application/json',
+        response=json.dumps(service_app_tools.is_service_or_app_running(type="app", name=request.data.decode())),
+        mimetype="application/json",
     )
 
 
-@app.route('/api/status_app', methods=['POST'])
+@app.route("/api/status_app", methods=["POST"])
 def status_app():
-    return service_app_tools.get_service_or_app_status(type='app', name=request.data.decode())
+    return service_app_tools.get_service_or_app_status(type="app", name=request.data.decode())
 
 
-@app.route('/api/app_disable')
+@app.route("/api/app_disable")
 def disable_app():
     return Response(
         response=json.dumps(service_app_tools.list_app_to_disable()),
-        mimetype='application/json',
+        mimetype="application/json",
     )
 
 
 # Dashboard API
-@app.route('/api/change-compliance', methods=['GET'])
+@app.route("/api/change-compliance", methods=["GET"])
 async def change_compliance():
-    part_req = request.args.get('part')
+    part_req = request.args.get("part")
 
-    compliance_req = request.args.get('compliance')
+    compliance_req = request.args.get("compliance")
 
-    if compliance_req == 'smooth':
+    if compliance_req == "smooth":
         await reachy_dashboard.turn_off_smoothly(part=part_req)
         return Response(status=200)
 
-    if compliance_req == 'false':
+    if compliance_req == "false":
         compliance_req = False
     else:
         compliance_req = True
 
-    reachy_dashboard.change_compliancy(
-        part=part_req,
-        compliance=compliance_req
-    )
+    reachy_dashboard.change_compliancy(part=part_req, compliance=compliance_req)
     return Response(status=200)
 
 
-@app.route('/api/get-compliance-config')
+@app.route("/api/get-compliance-config")
 def get_compliance_config():
     config = list(reachy_dashboard._compliance_config.keys())
     return Response(
         response=json.dumps(config),
-        mimetype='application/json',
+        mimetype="application/json",
     )
 
 
-@app.route('/api/get-fans-info')
+@app.route("/api/get-fans-info")
 def get_fans_info():
     return Response(
         response=json.dumps(reachy_dashboard.get_fans_info()),
-        mimetype='application/json',
+        mimetype="application/json",
     )
 
 
-@app.route('/api/set-fans-state', methods=['GET'])
+@app.route("/api/set-fans-state", methods=["GET"])
 def set_fans_state():
-    fan_req = request.args.get('fan')
-    state_req = request.args.get('state')
+    fan_req = request.args.get("fan")
+    state_req = request.args.get("state")
 
     reachy_dashboard.set_fan_state(fan=fan_req, state=state_req)
     return Response(status=200)
 
 
-@app.route('/api/get-states')
+@app.route("/api/get-states")
 def get_states():
+    logging.info(json.dumps(reachy_dashboard.get_states()))
     return Response(
         response=json.dumps(reachy_dashboard.get_states()),
-        mimetype='application/json',
+        mimetype="application/json",
     )
 
 
-@app.route('/api/get-reachy-info')
+@app.route("/api/get-reachy-info")
 def get_robot_info():
     return Response(
-        response=json.dumps({
-            'model': get_reachy_model(),
-            'serial_number': get_reachy_serial_number(),
-        }),
-        mimetype='application/json',
+        response=json.dumps(
+            {
+                "model": get_reachy_model(),
+                "serial_number": get_reachy_serial_number(),
+            }
+        ),
+        mimetype="application/json",
     )
 
 
 # Common tools API
-@app.route('/api/shutdown')
+@app.route("/api/shutdown")
 def shutdown():
     common_tools.shutdown()
     return Response(status=200)
 
+
 # Helper pages
-@app.route('/reconnect_motor')
+@app.route("/reconnect_motor")
 def reconnect_motor():
-    return render_template('reconnect_motor.html')
+    return render_template("reconnect_motor.html")
 
 
-@app.route('/reconnect_load_sensor')
+@app.route("/reconnect_load_sensor")
 def reconnect_load_sensor():
-    return render_template('reconnect_load_sensor.html')
+    return render_template("reconnect_load_sensor.html")
 
 
+@app.route("/motor_control")
+def control():
+    return render_template("control.html")
 
-if __name__ == '__main__':
 
+# server.py
+
+
+@app.route("/api/get_parts_and_joints")
+def get_parts_and_joints():
+    # Add min and max values for each joint
+    data = {
+        "r_arm": {
+            "r_shoulder_pitch": {"min": -150, "max": 90},
+            "r_shoulder_roll": {"min": -180, "max": 10},
+            "r_arm_yaw": {"min": -90, "max": 90},
+            "r_elbow_pitch": {"min": -125, "max": 0},
+            "r_forearm_yaw": {"min": -100, "max": 100},
+            "r_wrist_pitch": {"min": -45, "max": 45},
+            "r_wrist_roll": {"min": -55, "max": 35},
+            "r_gripper": {"min": -50, "max": 25},
+        },
+        "l_arm": {
+            "l_shoulder_pitch": {"min": -150, "max": 90},
+            "l_shoulder_roll": {"min": -10, "max": 180},
+            "l_arm_yaw": {"min": -90, "max": 90},
+            "l_elbow_pitch": {"min": -125, "max": 0},
+            "l_forearm_yaw": {"min": -100, "max": 100},
+            "l_wrist_pitch": {"min": -45, "max": 45},
+            "l_wrist_roll": {"min": -35, "max": 55},
+            "l_gripper": {"min": -25, "max": 50},
+        },
+    }
+
+    return json.dumps(data)
+
+
+@app.route("/api/trigger_event", methods=["POST"])
+def trigger_event():
+    data = request.get_json()
+    part = data["part"]
+    slider_values = data["sliderValues"]
+    duration = data["duration"]  # Get the duration from the received data
+
+    # Log the received data
+    logging.info(f"Received data for part {part} with slider values {slider_values} and duration {duration}")
+    reachy_dashboard = dashboard_tools.ReachyDashboard()
+    cmd = {k: float(v) for k, v, in slider_values.items()}
+    # cmd = {"r_elbow_pitch" : 45}
+    logging.info(cmd)
+    reachy_dashboard.set_goal_position(cmd)
+    # Your code to handle the event goes here
+
+    return Response(status=200)
+
+
+@app.route("/api/record_pose", methods=["POST"])
+def record_pose():
+    data = request.get_json()
+    part = data["part"]
+    slider_values = data["sliderValues"]
+    duration = data["duration"]
+    pose_name = data["poseName"]
+
+    logging.info(f"Received pose {pose_name} for part {part} with slider values {slider_values} and duration {duration}")
+
+    # Your code to handle the event goes here
+
+    return Response(status=200)
+
+
+@app.route("/api/set_joint_value", methods=["GET"])
+def set_joint_value():
+    part = request.args.get("part")
+    joint = request.args.get("joint")
+    value = request.args.get("value")
+
+    # This is where you call your function
+    # Assuming that your function expects the value to be a float in the range [0, 1]
+    value = float(value) / 100.0
+    goto_dict = {joint: value}
+    goto(goto_dict, duration=0.1)
+
+    return Response(status=200)
+
+
+if __name__ == "__main__":
     # time.sleep(10.0)
 
-    print('Starting Reachy dashboard...')
+    print("Starting Reachy dashboard...")
 
     net_tools = network_tools.NetworkTools()
 
-    connection_status = net_tools.get_connection_status()['mode']
+    connection_status = net_tools.get_connection_status()["mode"]
 
-    if not (connection_status == 'Wifi' or connection_status == 'Ethernet'):
-        net_tools.set_hotspot_state('off')
+    if not (connection_status == "Wifi" or connection_status == "Ethernet"):
+        net_tools.set_hotspot_state("off")
         wifi_list = net_tools.get_available_wifis()
-        net_tools.set_hotspot_state('on')
+        net_tools.set_hotspot_state("on")
 
     else:
         wifi_list = net_tools.get_available_wifis()
 
     net_tools.display_ip(net_tools.get_ip())
-    app.run(host='0.0.0.0', port=3972, debug=True)
+    app.run(host="0.0.0.0", port=3972, debug=True)
--- dashboard/tools/dashboard_tools.py
+++ dashboard/tools/dashboard_tools.py
@@ -15,25 +15,38 @@ from reachy_sdk.fan import Fan
 
 
 part_to_joints = {
-    'Left arm': [
-        'l_shoulder_pitch', 'l_shoulder_roll', 'l_arm_yaw',
-        'l_elbow_pitch', 'l_forearm_yaw', 'l_wrist_pitch',
-        'l_wrist_roll', 'l_gripper',
+    "Left arm": [
+        "l_shoulder_pitch",
+        "l_shoulder_roll",
+        "l_arm_yaw",
+        "l_elbow_pitch",
+        "l_forearm_yaw",
+        "l_wrist_pitch",
+        "l_wrist_roll",
+        "l_gripper",
     ],
-    'Right arm': [
-        'r_shoulder_pitch', 'r_shoulder_roll', 'r_arm_yaw',
-        'r_elbow_pitch', 'r_forearm_yaw', 'r_wrist_pitch',
-        'r_wrist_roll', 'r_gripper',
+    "Right arm": [
+        "r_shoulder_pitch",
+        "r_shoulder_roll",
+        "r_arm_yaw",
+        "r_elbow_pitch",
+        "r_forearm_yaw",
+        "r_wrist_pitch",
+        "r_wrist_roll",
+        "r_gripper",
     ],
-    'Head': [
-        'l_antenna', 'r_antenna', 'neck_roll',
-        'neck_pitch', 'neck_yaw',
+    "Head": [
+        "l_antenna",
+        "r_antenna",
+        "neck_roll",
+        "neck_pitch",
+        "neck_yaw",
     ],
 }
 
 
 class ReachyDashboard:
-    def __init__(self, host: str = 'localhost', sdk_port: int = 50055) -> None:
+    def __init__(self, host: str = "localhost", sdk_port: int = 50055) -> None:
         self._host = host
         self._sdk_port = sdk_port
         self._fans: Dict[str, Fan] = {}
@@ -44,7 +57,7 @@ class ReachyDashboard:
         self.joints = {}
 
         try:
-            self._grpc_channel = grpc.insecure_channel(f'{self._host}:{self._sdk_port}')
+            self._grpc_channel = grpc.insecure_channel(f"{self._host}:{self._sdk_port}")
         except _InactiveRpcError:
             self.connection_succeed = False
             return
@@ -57,7 +70,7 @@ class ReachyDashboard:
         resp = fans_stub.GetAllFansId(Empty())
         for name, uid in zip(resp.names, resp.uids):
             fan = Fan(name, uid, stub=fans_stub)
-            if name == 'neck_fan':
+            if name == "neck_fan":
                 continue
             self._fans[name] = fan
 
@@ -67,14 +80,14 @@ class ReachyDashboard:
         joint_ids = self.joint_stub.GetAllJointsId(Empty())
         self.joint_names = joint_ids.names
 
-        if 'l_shoulder_pitch' in self.joint_names:
-            self._parts.append('Left arm')
+        if "l_shoulder_pitch" in self.joint_names:
+            self._parts.append("Left arm")
 
-        if 'r_shoulder_pitch' in self.joint_names:
-            self._parts.append('Right arm')
+        if "r_shoulder_pitch" in self.joint_names:
+            self._parts.append("Right arm")
 
-        if 'l_antenna' in self.joint_names:
-            self._parts.append('Head')
+        if "l_antenna" in self.joint_names:
+            self._parts.append("Head")
 
         for part in self._parts:
             self.joints[part] = part_to_joints[part]
@@ -84,15 +97,31 @@ class ReachyDashboard:
     def change_compliancy(self, part: str, compliance: bool):
         all_req_joints = [self.joints[p] for p in self._compliance_config[part]]
         flat_joint_list = [joint for p in all_req_joints for joint in p]
-        cmd_msg = JointsCommand(
-            commands=[self._build_compliance_cmd_msg(joint, compliance) for joint in flat_joint_list]
-        )
+        cmd_msg = JointsCommand(commands=[self._build_compliance_cmd_msg(joint, compliance) for joint in flat_joint_list])
         self.joint_stub.SendJointsCommands(cmd_msg)
 
     def _build_compliance_cmd_msg(self, joint_name, compliance):
         msg = JointCommand(
             id=JointId(name=joint_name),
             compliant=BoolValue(value=compliance),
+        )
+        return msg
+
+    def set_goal_position(self, joint_goal_pos_dict):
+        self.joint_stub.SendJointsCommands(
+            JointsCommand(
+                commands=[
+                    self._build_goal_position_cmd_msg(joint_name, goal_position)
+                    for joint_name, goal_position in joint_goal_pos_dict.items()
+                ]
+            )
+        )
+
+    def _build_goal_position_cmd_msg(self, joint_name, goal_position):
+        msg = JointCommand(
+            id=JointId(name=joint_name),
+            compliant=BoolValue(value=True),
+            goal_position=FloatValue(value=goal_position),
         )
         return msg
 
@@ -107,21 +136,15 @@ class ReachyDashboard:
         all_req_joints = [self.joints[p] for p in self._compliance_config[part]]
         flat_joint_list = [joint for p in all_req_joints for joint in p]
 
-        cmd_msg = JointsCommand(
-            commands=[self._build_torque_limit_cmd_msg(joint, 0) for joint in flat_joint_list]
-        )
+        cmd_msg = JointsCommand(commands=[self._build_torque_limit_cmd_msg(joint, 0) for joint in flat_joint_list])
         self.joint_stub.SendJointsCommands(cmd_msg)
 
         await asyncio.sleep(2.0)
 
-        cmd_msg = JointsCommand(
-            commands=[self._build_compliance_cmd_msg(joint, True) for joint in flat_joint_list]
-        )
+        cmd_msg = JointsCommand(commands=[self._build_compliance_cmd_msg(joint, True) for joint in flat_joint_list])
         self.joint_stub.SendJointsCommands(cmd_msg)
 
-        cmd_msg = JointsCommand(
-            commands=[self._build_torque_limit_cmd_msg(joint, 100) for joint in flat_joint_list]
-        )
+        cmd_msg = JointsCommand(commands=[self._build_torque_limit_cmd_msg(joint, 100) for joint in flat_joint_list])
         self.joint_stub.SendJointsCommands(cmd_msg)
 
     def _set_part_compliance_config(self):
@@ -131,22 +154,22 @@ class ReachyDashboard:
             self._compliance_config[part] = [part]
 
         if len(self._parts) > 1:
-            self._compliance_config['Robot'] = self._parts
+            self._compliance_config["Robot"] = self._parts
 
-        if 'Left arm' and 'Right arm' in self._parts:
-            self._compliance_config['Both arms'] = ['Left arm', 'Right arm']
+        if "Left arm" and "Right arm" in self._parts:
+            self._compliance_config["Both arms"] = ["Left arm", "Right arm"]
 
     def get_fans_info(self):
         fans_info = {}
         for f in self._fans.values():
             if f.is_on:
-                fans_info[f.name] = 'on'
+                fans_info[f.name] = "on"
             else:
-                fans_info[f.name] = 'off'
+                fans_info[f.name] = "off"
         return fans_info
 
     def set_fan_state(self, fan, state):
-        if state == 'on':
+        if state == "on":
             self._fans[fan].on()
         else:
             self._fans[fan].off()
@@ -160,7 +183,7 @@ class ReachyDashboard:
             request = JointsStateRequest(ids=ids, requested_fields=[JointField.TEMPERATURE])
             res = self.joint_stub.GetJointsState(request)
 
-            for (joint_id, state) in zip(res.ids, res.states):
+            for joint_id, state in zip(res.ids, res.states):
                 temp_part[joint_id.name] = int(state.temperature.value)
 
             temperatures[part] = temp_part
@@ -173,20 +196,18 @@ class ReachyDashboard:
         for part in self.joints:
             state_part = {}
             ids = [JointId(name=joint) for joint in self.joints[part]]
-            request = JointsStateRequest(
-                ids=ids,
-                requested_fields=[JointField.TEMPERATURE, JointField.PRESENT_POSITION])
+            request = JointsStateRequest(ids=ids, requested_fields=[JointField.TEMPERATURE, JointField.PRESENT_POSITION])
             res = self.joint_stub.GetJointsState(request)
 
-            for (joint_id, state) in zip(res.ids, res.states):
+            for joint_id, state in zip(res.ids, res.states):
                 state_joint = {}
 
                 try:
-                    state_joint['temperature'] = int(state.temperature.value)
+                    state_joint["temperature"] = int(state.temperature.value)
                 except ValueError:
-                    state_joint['temperature'] = 0
+                    state_joint["temperature"] = 0
 
-                state_joint['position'] = int(rad2deg(state.present_position.value))
+                state_joint["position"] = int(rad2deg(state.present_position.value))
                 state_part[joint_id.name] = state_joint
 
             states[part] = state_part
